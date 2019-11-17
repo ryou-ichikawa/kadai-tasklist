ad06244d05307a8fb0605e6bebab7e2f5ac08994
ryou.ichikawa <kunren@kdk-nagano.co.jp>
2019-11-17 22:04:34 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         @include('commons.navbar')
         
         <div class="container">
-           @include('commons.error_messages')
+           @include('commons.error_tasks')
            
            @yield('content')
         </div>
