2244478e8784d2ee660fd3a8d6bda5d2ec5476f4
ryou.ichikawa <kunren@kdk-nagano.co.jp>
2019-11-21 22:00:29 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,9 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 // ユーザ機能
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+
+    
+    
 });
 
 
