d6a97158553425f0016e9b6740df5df9580fea66
ryou.ichikawa <kunren@kdk-nagano.co.jp>
2019-11-27 00:34:01 +0000

    TasksContoroller

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -79,7 +79,7 @@ class TasksController extends Controller
  
         if (\Auth::id() === $task->user_id) {
 
-        return view('task.show', [
+        return view('tasks.show', [
             'task' => $task,
         ]);
     } else {
