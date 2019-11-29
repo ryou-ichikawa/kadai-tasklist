f9c6cf59d3d218d92fdcca853bfd05491fdcbceb
ryou.ichikawa <kunren@kdk-nagano.co.jp>
2019-11-29 01:55:40 +0000

    Task.php

--- app/Task.php
+++ app/Task.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['id', 'status', 'content', 'user_id'];
+    protected $fillable = ['status', 'content', 'user_id'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-
+    
+    
 }
